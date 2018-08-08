2c14ecd7caa2fd896160f3052994d95ce28bc7e6
peter j <peter.jamack@pagarba.io>
2018-08-08 13:06:02 -0400

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
- var connect = require('connect')
-   , http = require('http')
-   , https = require('https');
-
- var app = connect();
-
- http.createServer(app).listen(6667);
- https.createServer(options, app).listen(443);
