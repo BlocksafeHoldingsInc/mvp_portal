31cad33874f636477f8f2380335a756848471d19
peter j <peter.jamack@pagarba.io>
2018-08-08 12:30:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
 res.writeHead(200);
-res.end('welcome to pagarba
+res.end('welcome to pagarba streamiot');
 });
-server.listen(6667)
+server.listen(6667);
