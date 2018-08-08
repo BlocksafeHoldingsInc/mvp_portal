a7a7146006e467d2cbe07110c69e7c050db5b045
peter j <peter.jamack@pagarba.io>
2018-08-08 12:50:55 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
-//nodejs
-
-var instructionsNewVisitor = function(req, res) {
-res.writeHead(200);
-res.end('Hi, welcome to pagarba streamiot ');
-}
-
-var server = http.createServer(instructionsNewVisitor);
-
-server.listen(6667);
+var http = require('http');  
+var url = require('url');  
+var fs = require('fs');  
+var server = http.createServer(function(request, response) {  
+    var path = url.parse(request.url).pathname;  
+    switch (path) {  
+        case '/':  
+            response.writeHead(200, {  
+                'Content-Type': 'text/plain'  
+            });  
+            response.write("This is Test... Your Money is all gone... Sorry.. Try Again...");  
+            response.end();  
+            break;  
+        case '/index.html':  
+            fs.readFile(__dirname + path, function(error, data) {  
+                if (error) {  
+                    response.writeHead(404);  
+                    response.write(error);  
+                    response.end();  
+                } else {  
+                    response.writeHead(200, {  
+                        'Content-Type': 'text/html'  
+                    });  
+                    response.write(data);  
+                    response.end();  
+                }  
+            });  
+            break;  
+        default:  
+            response.writeHead(404);  
+            response.write("opps this doesn't exist - 404");  
+            response.end();  
+            break;  
+    }  
+});  
+server.listen(6667, '0.0.0.0');
+server.on('listening', function() {
+    console.log('Express server started on port %s at %s', server.address().port, server.address().address);
+ });
